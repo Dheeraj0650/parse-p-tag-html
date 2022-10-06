f65b51f23ea2cb1e3b0f2269718c1c4d029accf3
Dheeraj0650 <kottedheeraj@gmail.com>
2022-10-06 17:14:49 -0600

    Initial commit

---
+++ spec/parse-p-tag-html-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ParsePTagHtmlView from '../lib/parse-p-tag-html-view';
+
+describe('ParsePTagHtmlView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
